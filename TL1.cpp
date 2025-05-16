3ac037be4bf35052a45a55d76d5354abaf2ae340
TsubokiHayato <k023g0088@g.neec.ac.jp>
2025-05-16 11:44:18 +0900

    Blenderアドオンの基本構造を追加
    `bpy`モジュールのインポートと`bl_info`辞書を追加し、アドオンの基本情報を定義しました。`register`および`unregister`関数を実装し、アドオンの有効化/無効化時にメッセージを出力する処理を追加しました。また、スクリプトが直接実行された場合のテストコードを追加しました。

--- TL1.cpp
+++ TL1.cpp
@@ -1,1 +1,26 @@
-﻿
+﻿import bpy
+
+#ブレンダーに登録するアドオン情報
+bl_info={
+    "name":"レベルエディタ",
+    "author":"Hayato Tsuboki",
+    "version": (1,0),
+    "blender":(3,3,1),
+    "location":"",
+    "description": "レベルエディタ",
+    "warning":"",
+    "wiki_url":"",
+    "tracker_url":"",
+    "category":"Object"
+}
+#アドオン有効化時コールバック
+def register():
+    print("レベルエディタが有効化されました。")
+
+#アドオン無効化時コールバック
+def unregister():
+    print("レベルエディタが無効化されました。")
+    
+#test
+if __name__=="__main__":
+    register()
